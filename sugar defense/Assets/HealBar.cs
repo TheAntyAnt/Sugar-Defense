f22961f67a4076879513e41e5d7b342662039c92
Onion-Bro <Lucas.Lindgren@elev.ga.lbs.se>
2023-04-12 11:49:23 +0200

    Health bar start
    WI

---
+++ sugar defense/Assets/HealBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealBar : MonoBehaviour
+{
+    public float Heal;
+    public string Fool;
+    public Transform Fill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fill.localScale = new Vector3(Heal * 0.01, 0, 0);
+    }
+}
