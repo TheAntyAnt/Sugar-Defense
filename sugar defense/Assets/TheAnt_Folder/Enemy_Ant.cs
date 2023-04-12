33957722940e45f60b80f8ccf296fcccf70ef421
TheAntyAnt <fabian.borg.04@hotmail.se>
2023-04-12 11:44:55 +0200

    don't worry about this

--- sugar defense/Assets/TheAnt_Folder/Enemy_Ant.cs
+++ sugar defense/Assets/TheAnt_Folder/Enemy_Ant.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-
 public class Enemy_Ant : MonoBehaviour
 {
 
@@ -13,11 +9,16 @@ public class Enemy_Ant : MonoBehaviour
     [SerializeField] private int health;
     [SerializeField] private int damage;
     [SerializeField] private int speed;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //create array named "waypoints"
+        GameObject[] waypoints;
+        //tells the array to find any gameobjects with the tag "Waypoint"
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        Debug.Log(waypoints.Length - 1);
     }
 
     // Update is called once per frame
--- sugar defense/Assets/TheAnt_Folder/Tower.cs
+++ sugar defense/Assets/TheAnt_Folder/Tower.cs
@@ -11,11 +11,9 @@ public class Tower : MonoBehaviour
     //define the range of the tower
     public float range = 15f;
 
-    //create a tag that you can assign to enemies
-    public string enemyTag = "enemy";
 
     //what part of the tower should spin?
-    public Transform partToRotate;
+    public Transform baset;
     public float rotationSpeed = 10f;
 
     // Start is called before the first frame update
@@ -29,7 +27,7 @@ public class Tower : MonoBehaviour
     void UpdateTarget()
     {
         //Creates an array called "Enemies" that will find any GameObjects 
-        GameObject[] Enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        GameObject[] Enemies = GameObject.FindGameObjectsWithTag("enemy");
         //stores the distance of the closest enemy
         float shortestDistance = Mathf.Infinity;
         //stores which enemy is closest
@@ -41,7 +39,7 @@ public class Tower : MonoBehaviour
             //looks for the distance between the enemy and the tower
             float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
             //checks to see if the distance of a new enemy is closer than the previous distance checked
-            if (distanceToEnemy > shortestDistance && nearestEnemy == null)
+            if (distanceToEnemy > shortestDistance)
             {
                 shortestDistance = distanceToEnemy;
                 nearestEnemy = enemy;
@@ -51,6 +49,7 @@ public class Tower : MonoBehaviour
         if (nearestEnemy != null && shortestDistance <= range)
         {
             target = nearestEnemy.transform;
+            Debug.Log("I found the enemy!");
         }
         else
         {
@@ -66,7 +65,7 @@ public class Tower : MonoBehaviour
         {
             return;
         }
-
+        
         //what direction is the target?
         Vector3 dir = target.position - transform.position;
         //How to turn to look in the direction of the target
@@ -74,10 +73,10 @@ public class Tower : MonoBehaviour
         //turn the complicated quaternion equation for rotation into nice and simple eulerAngles
         //eulerAngles is when you rotate something around an axis
         //Lerp makes it so it turns smoothly over a time
-        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
+        Vector3 rotation = Quaternion.Lerp(baset.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
         //makes it so it only rotates around the Y-axis
-        partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
-
+        baset.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+        
     }
     
     //visualize the range of the tower with a wire and make it blue
