80cb404773d951d339bab808e93fd7b27aa50403
Undergrid04 <77395287+Undergrid04@users.noreply.github.com>
2023-04-19 14:16:56 +0200

    Health

--- sugar defense/Assets/Gurkan/Health.cs
+++ sugar defense/Assets/Gurkan/Health.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+
+    public float maxhealth = 100f;
+    public float currenthealth;
+
+    public float damage;
+    public float healing;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currenthealth = maxhealth;
     }
 
     // Update is called once per frame
