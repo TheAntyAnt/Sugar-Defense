74fcfdf15c4a77861a4043bec1c253705988ce71
TheAntyAnt <fabian.borg.04@hotmail.se>
2023-04-26 14:26:54 +0200

    Tornet roterar nu

--- sugar defense/Assets/TheAnt_Folder/Tower.cs
+++ sugar defense/Assets/TheAnt_Folder/Tower.cs
@@ -23,16 +23,7 @@ public class Tower : MonoBehaviour
     {
         mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
-        
-        //what direction is the mousepointer?
-        Vector3 lookPos = mousePos - transform.position;
 
-        Quaternion lookRotation = Quaternion.LookRotation(lookPos);
-        //turn the complicated quaternion equation for rotation into nice and simple eulerAngles
-        //eulerAngles is when you rotate something around an axis
-        //Lerp makes it so it turns smoothly over a time
-        Vector3 rotation = Quaternion.Lerp(RotationPoint.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
-        //makes it so it only rotates around the Z-axis
-        RotationPoint.rotation = Quaternion.Euler(rotation.x, 0f, 0f);
+        transform.up = mousePos;
     }
 }
